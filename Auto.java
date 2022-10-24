b875417967366580caf56fd7e918acffa82d26e2
Nahuel Blasua <nahuelbs95@gmail.com>
2022-10-24 02:45:39 -0300

    Estado de un OBJETO(1)

--- Auto.java
+++ Auto.java
@@ -1,3 +1,9 @@
 public class Auto {
     
+    //Atributos de autos
+    String patente;
+    String color;
+    int kms;
+    boolean conAire;
+    //Estos son los atributos que todo auto deberia tener cuando creemos objetos en java, de tipo auto
 }
--- Principal.java
+++ Principal.java
@@ -2,10 +2,24 @@ public class Principal {
     
     public static void main(String[] args) {
         
-        Auto miAuto = new Auto();//Se crea un objeto llamado mi auto de Clase Auto 
+        new Auto();//creamos un objeto auto,pero al no estar guardado,asi como lo creamos lo perdemos
+
+        //para guardar un objeto lo asociamos a una variable dicha variable tiene que ser declarada del mismo tipo que el objeto
+
+        Auto a;//creo variable tipo Auto
+
+        a=new Auto();//creo un objeto auto y la asocio a una variable tipo auto
+
+        System.out.println(a.color + " " + a.patente + " " + a.kms + " " + a.conAire);//Observamos si el objeto tiene o no estado(valor de los atributos en el instante que lo estamos consultando)
+
     }
 }
 
-//Las Clases deben llamarse con la primera letra en Mayuscula 
+//Lo que mostrara en la terminal sera null null 0 false 
 
-//Las Clases se guardan en archivos independientes,dichos archivos deben coincidir con el nombre de la clase
+//¿En que momento nosotros dijimos que tenian que valer eso? Java le asigna un estado inicial a todo objeto de cualquier tipo
+
+//En el caso que un atributo apunte a otro objeto(String y demas tipos de objeto) le pondra como valor NULL(los String son objetos,vemos que estos son tratados como referancia no como cadena vacia,es null,no hay ninguna cadena alli guardada)
+//Para el caso de una variable numerica(byte,short,int,long,float,double,char) es 0
+//Para los tipo logicos(boolean) el valor por defecto sera falso
+
