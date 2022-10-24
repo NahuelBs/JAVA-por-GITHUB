1b04b7f1a0a5b758749f7876dc1ec5641e2f5514
Nahuel Blasua <nahuelbs95@gmail.com>
2022-10-23 22:36:05 -0300

    Creando nuestra primera clase

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Auto miAuto = new Auto();//Se crea un objeto llamado mi auto de Clase Auto 
+    }
+}
+
+//Las Clases deben llamarse con la primera letra en Mayuscula 
+
+//Las Clases se guardan en archivos independientes,dichos archivos deben coincidir con el nombre de la clase
