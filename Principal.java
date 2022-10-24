3a28ada86b60143abb5655d2efc36e6630aa71d4
Nahuel Blasua <nahuelbs95@gmail.com>
2022-10-24 02:56:20 -0300

    Estado de un OBJETO(2)

--- Principal.java
+++ Principal.java
@@ -2,24 +2,39 @@ public class Principal {
     
     public static void main(String[] args) {
         
-        new Auto();//creamos un objeto auto,pero al no estar guardado,asi como lo creamos lo perdemos
+        new Auto();
 
-        //para guardar un objeto lo asociamos a una variable dicha variable tiene que ser declarada del mismo tipo que el objeto
+        Auto a;
 
-        Auto a;//creo variable tipo Auto
+        a=new Auto();
 
-        a=new Auto();//creo un objeto auto y la asocio a una variable tipo auto
+        System.out.println(a.color + " " + a.patente + " " + a.kms + " " + a.conAire);
 
-        System.out.println(a.color + " " + a.patente + " " + a.kms + " " + a.conAire);//Observamos si el objeto tiene o no estado(valor de los atributos en el instante que lo estamos consultando)
+        //Realizaremos un cambio de estado,es decir cambiamos los valores que vienen por defecto
 
+        a.patente="ABC123";
+        a.color="Verde";
+        a.kms=500;
+        a.conAire=true;
+
+        System.out.println(a.color + " " + a.patente + " " + a.kms + " " + a.conAire);
+
+        //Al principio de la consola,salio el estado por defecto y luuego el mismo objeto cambio de estado,paso a tener los valores que nosotros acabamos de poner
+
+        //Crearemos un segundo auto(un segundo objeto)
+        Auto a2 = new Auto();
+        System.out.println(a2.color + " " + a2.patente + " " + a2.kms + " " + a2.conAire);//Observamos el estado del auto 2
+        
+        //Se observa al correr el programa que para el auto dos vuelve a salir los valores por defecto
+        
+        //Ponemos un nuevo estado al segundo auto
+        a2.patente="DDFG123";
+        a2.color="Azul";
+        a2.kms=100;
+        a2.conAire=true;
+
+        System.out.println(a2.color + " " + a2.patente + " " + a2.kms + " " + a2.conAire);//Observamos el estado del auto 2    
+    
     }
 }
-
-//Lo que mostrara en la terminal sera null null 0 false 
-
-//¿En que momento nosotros dijimos que tenian que valer eso? Java le asigna un estado inicial a todo objeto de cualquier tipo
-
-//En el caso que un atributo apunte a otro objeto(String y demas tipos de objeto) le pondra como valor NULL(los String son objetos,vemos que estos son tratados como referancia no como cadena vacia,es null,no hay ninguna cadena alli guardada)
-//Para el caso de una variable numerica(byte,short,int,long,float,double,char) es 0
-//Para los tipo logicos(boolean) el valor por defecto sera falso
 
