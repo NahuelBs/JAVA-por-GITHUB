f786e2b6f636c468fa195cc29dc33b5a8edd2b3f
Nahuel Blasua <nahuelbs95@gmail.com>
2022-10-25 03:18:05 -0300

    Identidad de un objeto(6)

--- Principal.java
+++ Principal.java
@@ -13,23 +13,13 @@ public class Principal {
 
         Auto a2;
         
-        a2=a;
+        a2=null;//ahora en lugar de guardar a en a2 guardamos null
 
         System.out.println(a.color);
-        a2.color="Azul";
-        System.out.println(a.color);
-
-        //Al ejecutar el programa nosotros primero veremos Verde y luego azul,por que sucede esto si yo cambie "a2" y no "a"?
-        //Lo que sucede que al igualar las dos variables estas apuntaran al mismo objeto
-
-        System.out.println(a);
-        System.out.println(a2);//lo anteriormente dicho se lo puede observar con las lineas 25 y 26
-
-        //Vemos en la terminal la misma identidad ,es decir entonces que en la memoria hay un solo objeto
-        //ese objeto esta una unica vez y habra dos manera de llegar a este objeto,por medio de la variable a o a2
         
-        //Un ejemplo de lo anterior,si uno pasa la direccion de una casa a distintas personas,todos tienen la misma direccion para distintas personas,
-        //y si por ejemplo pintamos la casa este cambio se ve reflejado a todos que lleguen a ella por medio de su direccion
+        a2.color="Azul";//cuando queremos que a2 acceda a su color saldra el error NullPointerException(es decir estamos tratando de acceder a un campo a traves de un punteo nulo,es decir una referencia nula)
+
+        System.out.println(a.color);
 
     }
 }
