b16cf8f1adf844ed23a784c2120aa60761275f24
Nahuel Blasua <nahuelbs95@gmail.com>
2022-10-25 03:14:15 -0300

    Identidad de un objeto(5)

--- Principal.java
+++ Principal.java
@@ -12,14 +12,26 @@ public class Principal {
         a.conAire=true;
 
         Auto a2;
-        System.out.println(a2);//observamos que nos sale error al querer mostrar a2,y nos dice que dicha variable no esta inicializada
-        //que no este inicializada nos idica que NO tiene un valor por defecto  
+        
+        a2=a;
 
-        a2=a;//Ahora en a2 guardamos a
+        System.out.println(a.color);
+        a2.color="Azul";
+        System.out.println(a.color);
 
-        System.out.println(a2);//observemos que para mostrar a2 en esta line,no sale con error eso sucede ya que la estoy inicializando antes es decir le di un valor para que pueda mostrar
+        //Al ejecutar el programa nosotros primero veremos Verde y luego azul,por que sucede esto si yo cambie "a2" y no "a"?
+        //Lo que sucede que al igualar las dos variables estas apuntaran al mismo objeto
+
+        System.out.println(a);
+        System.out.println(a2);//lo anteriormente dicho se lo puede observar con las lineas 25 y 26
+
+        //Vemos en la terminal la misma identidad ,es decir entonces que en la memoria hay un solo objeto
+        //ese objeto esta una unica vez y habra dos manera de llegar a este objeto,por medio de la variable a o a2
+        
+        //Un ejemplo de lo anterior,si uno pasa la direccion de una casa a distintas personas,todos tienen la misma direccion para distintas personas,
+        //y si por ejemplo pintamos la casa este cambio se ve reflejado a todos que lleguen a ella por medio de su direccion
+
     }
 }
-
 
 
