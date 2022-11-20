ac2decfab7d3168ed88db0849d0ef547b6156d6b
Nahuel Blasua <nahuelbs95@gmail.com>
2022-11-20 18:24:32 -0300

    PUBLIC y PRIVATE en Java:Cuando y Porque(1)

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,14 +1,14 @@
 public class CuentaBancaria {
     
+    
     String CBU;
     String alias;
     double saldo;
 
-    //creamos un metodo para mostrar el estado de la cuenta bancaria,asi no repetir tantas veces la misma linea como en el caso de la linea 6 y 9 de la clase principal
 
-    void mostrarDatos(){//todos los objetos cuenta bancaria podran realizar este metodo
+    void mostrarDatos(){
 
-        System.out.println(CBU +" "+ alias +" "+ saldo);//un metodo de la clase cuenta bancaria es directamente accesible a sus atributos
+        System.out.println(CBU +" "+ alias +" "+ saldo);
 
     }
 
@@ -22,20 +22,17 @@ public class CuentaBancaria {
             saldo=saldo+monto;
     }
     
-    //Nuestra ultima variante posible es un metodo que reciba un parametro y devuelva algo
 
-    boolean saldoDisponible(double monto){//El monto de este metodo no es el mismo que monto del metodo depositar son distintos parametros que yo decidi que se llamen iguales pero podrian llamarce de distintas maneras
+    boolean saldoDisponible(double monto){
         
-        return saldo >= monto;//saldo >= monto ya es una expresion logica es un booleano,java devolvera true o false si se cumple o no la expresion logica
+        return saldo >= monto;
     }
     
-    void extraer(double monto){//extrear lo hacemos void ya que no estamos simulando un cajero donde debemos tener la plata,solo queremos mostrar como cambia el estado de la cuanta
+    void extraer(double monto){
 
-        if(saldoDisponible(monto))//para no repetir logica de escribir de vuelta saldo >= monto ,se puede reemplazar por el metodo saldoDisponible
+        if(saldoDisponible(monto))
             saldo=saldo-monto;
 
-    }//En void extraer recibimos como parametro la variable monto,esa variable la utilizamos
-    //como argumento para el metodo saldoDisponible,saldoDisponible mandara un true o un false si se comple o no la expresion,y de ahi entrara al if si es true  
+    }
 
-    //Podemos llamarlo solamente con el nombre,sin necesidad de agragar una variable.saldoDisponible ya que nos encontramos en LA MISMA CLASE
 }
--- Principal.java
+++ Principal.java
@@ -1,36 +1,17 @@
 public class Principal {
     public static void main(String[] args) {
       
-        CuentaBancaria cb = new CuentaBancaria();//creamos un objeto cuenta bancaria,guardandola en la variable cb para no perderla asi llegar a el
+        CuentaBancaria cb = new CuentaBancaria();
 
-        //invocamos al metodo creado,podemos invocarlo las veces que uno quiera
-        cb.mostrarDatos();
-        cb.CBU="324788324629102";//realizamos dos cambios de estado
-        cb.alias="miCuenta";
-        cb.mostrarDatos();
-
-        cb.obtenerSaldo();
-
-
-        System.out.println(cb.obtenerSaldo());
-        cb.depositar(200);
-        System.out.println(cb.obtenerSaldo());
-        cb.depositar(-50);
-        System.out.println(cb.obtenerSaldo());
-
-        //Lo siguiente no es una extraccion si no mas bien es una consulta
-
-        System.out.println(cb.saldoDisponible(100));//Al ejecutar me saldra, para este caso true ya que mi saldo es 200 y yo pregunto si saldo es mayor al monto solicitado, en este caso 100
-        System.out.println(cb.saldoDisponible(300));//Al ejecutar me saldra, para este caso false ya que mi saldo es 200 y yo pregunto si saldo es mayor al monto solicitado,en este caso 300 
-
-        cb.extraer(180);
-        System.out.println(cb.obtenerSaldo());
-        cb.extraer(100);
-        System.out.println(cb.obtenerSaldo());
+        //Vemos que para este caso es asi de simple asignar los atributos a la variable cb
+        cb.saldo = -123;
+        cb.alias = ")/&(&$))";
+        cb.CBU = "Hola que tal";
+        //Dichos valores no son muy consistentes,es decir son cualquiera 
         
-        //Al ejecutar observamos que podremos extraer 180 ya que es menor al saldo(200) asi que el metdos saldoDisponible devuelve un true y entra al if del metodo extraer
-        //Pero para el segundo caso al ya extraer 180 nos queda 20 como saldo y el monto para extraer (100) es mayor al saldo disponible por este motivo saldoDisponible nos devuelve un false por ende no ingresamos al if del metodo extraer
-        //por eso al volver a llamar el metodo obtenerSaldo tenemos los 20 que teniamos antes
-
+        //Todo eso es posible ya que los atributos de la Cuenta Bancaria no son privados
+    
+        
+        cb.mostrarDatos();//por eso al llevar uso del metodo mosrarDatos veremos que en este momento la cuenta bancaria,no dara errores por que son valores posibles pero dichos valores son inconsistentes para emplear en una cuenta del banco
     }
 }
