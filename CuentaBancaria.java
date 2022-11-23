c27bbb88964324dbe82946cb24f7ef7afcbbfdb7
Nahuel Blasua <nahuelbs95@gmail.com>
2022-11-23 18:54:58 -0300

    Constructor en Java

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,106 +1,31 @@
 public class CuentaBancaria {
     
+   private String CBU;
+   private String alias;
+   private double saldo;
+   private String moneda;
+   private String tipo;
 
-    private String CBU;
-    
-    //getter y setter generado automaticamente para CBU    
+   public void mostrarDatos(){
+        System.out.println(CBU+" "+alias+" "+saldo+" "+moneda+" "+tipo);
+   }
 
-    public String getCBU() {
-        return CBU;
-    }
+   public void setAlias(String alias){
+        if(alias!=null)
+        this.alias=alias;
+   }
 
-    public void setCBU(String cBU) {
-        CBU = cBU;
-    }
-
-
-    private String alias;
-
-    //getter y setter generado automaticamente para alias
-    
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-
-    private double saldo;
-
-    //getter y setter generado automaticamente para saldo
-    
-    public double getSaldo() {
-        return saldo;
-    }
-
+   public void setMoneda(String moneda){
+        if(moneda!=null)
+            this.moneda=moneda;
+   }
    
+   public void setTipo(String tipo){
+        if(tipo!=null)
+            this.tipo=tipo;
+   }
 
-
-
-
-    void mostrarDatos(){
-
-        System.out.println(this);//linea para ver que es this
-        System.out.println(CBU +" "+ alias +" "+ saldo);
-
-    }
-
-    //El uso del this se da en circunstancias bastantes especificas una de ellas es lo que hacemos en la linea 47 ,el this hace referencia a la misma clase que esta siendo llamada en 
-    //este caso cuentabancaria para ser llamada dentro de un metodo
-
-
-    //Otro caso particular seria el siguiente
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;//utilizo el this ya que el parametro tiene el mismo nombre que el atributo de la clase
-
-    } 
-
-
-   
-    void establecerAlias(String nuevoAlias){
-        if(nuevoAlias!=null)
-            alias=nuevoAlias;
-    }
-    
-    String obtenerAlias(){
-        return this.alias;//para los metodos que no tiene ambiguedad da lo mismo si va o no el this 
-    }
-
-    double obtenerSaldo(){
-        
-        return saldo;
-    }
-
-    void depositar(double monto){
-        if(this.monto > 0)
-            saldo=saldo+this.monto;//pondriamos en un lugar incorrecto el this ya que monto  es el nombre que se decidio darle  al parametro de tipo double que llega desde afuera 
-            //para realizar un deposito,pero no existe NINGUN ATRIBUTO EN LA CLASE CUENTABANCARIA que se llame monto,es decir si la variable no se encuentra como atributo de la clase no podre usar this en ella
-    }
-    
-
-    private boolean saldoDisponible(double monto){
-        
-        return saldo >= monto;
-    }
-    
-    void extraer(double monto){
-
-        if(saldoDisponible(monto))
-            saldo=saldo-monto;
-
-    }
-
-
-    
-
+   public double getSaldo(){
+    return saldo;
+   }
 }
-
-//Al poner los atributos privados no podemos acceder a ellos en otra clase,pero si a treves de metodos
-
-//los getters son los metodos que devuelven los valores de los atributos como el obtenersaldo y obtener alias
-
-//los setters permite a un atributo setearlo con un nuevo valor
-
--- Principal.java
+++ Principal.java
@@ -2,12 +2,30 @@ public class Principal {
     public static void main(String[] args) {
       
         CuentaBancaria cb = new CuentaBancaria();
+        CuentaBancaria cb2 = new CuentaBancaria();
+        CuentaBancaria cb3 = new CuentaBancaria();
+
+        cb.setAlias("miAlias");
+        cb.setMoneda("Pesos");
+        cb.setTipo("Caja de ahorros");
+
+        cb2.setAlias("caja2");
+        cb2.setMoneda("Dolares");
+        cb2.setTipo("Caja de ahorros");
+
+        cb3.setAlias("cuenta_123");
+        cb3.setMoneda("Pesos");
+        cb3.setTipo("Cuenta corriente");
 
         cb.mostrarDatos();
-        System.out.println(cb);
-        cb.setSaldo(256);
-        cb.mostrarDatos();
+        cb2.mostrarDatos();
+        cb3.mostrarDatos();
+
     }
+    //vemos que crear algo tan sencillo como crear tres objetos del tipo cuenta bancaria y establecer parcialmente sus atributos  no lleva bastantes lineas de codigos
+    // y muchas de ellas repetidas setTipo,set Alias por mas que tenga valores diferentes
+    
+    //para evitar esto se utiliza lo que se conoce como un constructor,que nos va a permitir en una sola linea,no solamente construir el objeto si no tambien darle su estado inicial
 }
 
 
