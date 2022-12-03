f67d44324f88969883039b9d9241338c92be0560
Nahuel Blasua <nahuelbs95@gmail.com>
2022-12-03 01:33:49 -0300

    Sobrecarga de constructores (3)

--- Principal.java
+++ Principal.java
@@ -2,9 +2,10 @@ public class Principal {
     public static void main(String[] args) {
       
         CuentaBancaria cb = new CuentaBancaria("miAlias","Pesos","Caja de ahorros");
-        CuentaBancaria cb2 = new CuentaBancaria();//sale error por que no le estoy mandando los argumentos que declare en el constructor
+        CuentaBancaria cb2 = new CuentaBancaria();
 
         cb.mostrarDatos();
+        cb2.mostrarDatos();
     }
     
 }
