ee2c6051dccad3a571c16247e5399216698c19fe
Nahuel Blasua <nahuelbs95@gmail.com>
2022-12-03 15:20:00 -0300

    Sobrecarga de constructores (7)

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -28,10 +28,19 @@ public CuentaBancaria(){
 public CuentaBancaria(String moneda,String tipo){
      //Vamos a invocar desde aqui a otro constructor de la misma clase usando la palabra THIS,pero esta vez con parentesis
      
-     this(alias,moneda,tipo);//Siempre se debe apuntar al constructor mas grande que alla y resolviendo el parametro que falta,en este caso el alias
+     this("ALIAS POR DEFECTO",moneda,tipo);//Siempre se debe apuntar al constructor mas grande que alla y resolviendo el parametro que falta,en este caso el alias
      
-     //vemos que sale error en alias por que el constructor solo recibe dos variables
+     //para evitar el error ponemos un alias por defecto
 }
+
+//que nos permite esto? que cuando alguien haga new cuentabancaria y nos envie apenas 2 parametros de tipo String  se ejecutara el construtor con dos parametros es decir el de la linea 28
+//y asi como lo recibe los delega al construtor de la linea 13,gracias al this,es decir la moneda y y el tipo entran al constructor con dos parametros yo dentro de ese constructor llamo,
+//al constructor de 3 parametros mandando exactamente la misma moneda y tipo del comienzo,pero agregando un alias para el constructor de 3 parametros desde el construtor de dos parametros 
+
+
+//Entonces esto nos permite que los constructores mas pequeños,pueden llamar a los constructores  con mas parametros,por medio del THIS
+
+
 
    private String generarCBU(){
 
